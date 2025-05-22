6793e53f8150ae7b5c6bbc68b60a29b370eb335f
ankitagrg <ankitalg18@gmail.com>
2025-05-22 12:58:20 +0545

    Add: Prisma schema and migration files for Quote model changes

---
+++ backend/prisma/migrations/20250521020511_added_new_data/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Quote` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userID` to the `Quote` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Quote" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "text" TEXT NOT NULL,
+    "userID" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Quote" ("id", "text") SELECT "id", "text" FROM "Quote";
+DROP TABLE "Quote";
+ALTER TABLE "new_Quote" RENAME TO "Quote";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
