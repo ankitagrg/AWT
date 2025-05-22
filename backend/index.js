3f43d8c2061f331cc4eba53d9ec9782156dc8b20
ankitagrg <ankitalg18@gmail.com>
2025-05-22 12:57:49 +0545

    Refactor: Backend index.js with quote routes and express setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,38 @@
 // index.js
 const express = require('express');
-const server = express();
+const db = require("better-sqlite3")("data.db");
+const app = express();
 
-server.get('/', (request, response) => {
+app.use(express.json());
+
+app.get('/', (request, response) => {
   response.send('Hello');
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+
+  // Get all quotes//
+app.get('/api/users', (request, response) => {
+});
+
+// Create table //
+db.prepare(" CREATE TABLE IF NOT EXIST  quotes(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)"
+).run();
+app.get("quotes",(request, response)=>{
+  const all = db.prepare ("SELECT * FROM quotes").all();
+  response.json(all);
+});
+
+
+//All new quotes//
+app.post("/quotes",(request, response)=> {
+  const {text} = request.body;
+  if (!text) {
+    return response.status(400).json({ error: 'Quote text is required.' });
+  }
+
+  const info = db.prepare ("INSERT INTO quotes (text) VALUES(?)").run(text);
+  response.status(201).json({id: info.lastInsertRowid, text});
+});
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
